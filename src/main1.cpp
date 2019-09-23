1476ee3ff3c2f5e7619fecda325db3f0df752b75
Korkunov Anton <korkunov_a@list.ru>
2019-09-23 18:27:57 +0300

    tt

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,15 +1,17 @@
+﻿
 #include <iostream>
 #include "task1.h"
 #define _CRT_SECURE_NO_WARNINGS
 
-void main() {
+int main() {
 	int a = 0;
 	printf("Enter your age ");
-	scanf_s("%d",&a);
-	if(ageCheck(a)){
-	printf("Access granted");
+	scanf_s("%d", &a);
+	if (ageCheck(a)) {
+		printf("Access granted");
 	}
-    else{
-	printf("Access denied");
+	else {
+		printf("Access denied");
 	}
+	return 1;
 }

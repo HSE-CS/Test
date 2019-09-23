602aacb399808fae8827c8e8fa5872e0cd1c1496
Anghellia <son0shad@gmail.com>
2019-09-23 19:01:51 +0300

    first change

---
+++ src/main1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "task1.h"
+using namespace std;
+
+int main()
+{
+	int age;
+	cout << "Hello! Please, enter your age.\n";
+	cin >> age;
+	ageCheck(age);
+	return 0;
+}

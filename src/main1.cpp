8bbe49f1af518a437d7615328574840e678d8dde
Katya Malysheva <katyamalysheva@yandex.ru>
2019-09-25 00:51:10 +0300

    made by KM

---
+++ src/main1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "enter your age:";
+	cin >> x;
+	if (ageCheck(x) == 1)
+		cout << "Access granted";
+	else
+		cout << "Access denied";
+	
+	return 0;
+}

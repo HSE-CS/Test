7135d8d4e58c80dad014bd0e4180d88adb6b859c
anastasiaudachina <55730777+anastasiaudachina@users.noreply.github.com>
2019-09-25 08:12:12 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -1,14 +1,2 @@
-
-#ifndef TASK1_H
-#define TASK1_H
-
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h> 
-#include <conio.h>
-#include <string.h>
-#include <io.h>
-
-int ageCheck(int age);
-#endif
+#pragma once
+int ageCheck(int age); 

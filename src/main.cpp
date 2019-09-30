7fbf5b44490ac6d8dcd0e23dc239cb4fe3190fa0
Pavel <zamelinpavel@gmail.comgit config --global user.email zamelinpavel@gmail.comgit config --global user.email zamelinpavel@gmail.com>
2019-09-30 15:04:07 +0300

    acompl

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,9 @@ int main()
 	int age = 0;
 	scanf("%i", &age);
 	if (ageCheck(age) == 1)
-		printf("Access granted");
-	else printf("Access denied");
+		printf("Access granted\n");
+	else 
+                printf("Access denied\n");
 
 	return 0;
 }
--- src/task1.cpp
+++ src/task1.cpp
@@ -8,5 +8,6 @@ int ageCheck(int age)
 {
 	if (age >= 18)
 		return 1;
-	return 0;
+        else
+	        return 0;
 }

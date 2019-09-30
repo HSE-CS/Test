9868997bbab33959e706b7aa3d5724cdffa707f1
Mariia Glazova <35651375+MDAUBEITEMANYA@users.noreply.github.com>
2019-09-30 14:31:05 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,23 +1,13 @@
-// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
-//
-
-#include "stdafx.h"
 #include <iostream>
 #include "task1.h"
-using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	int age;
-	cin >> age;
-	if (ageCheck(age))
-	{
-		cout << "Access granted";
-	}
-	else
-	{
-		cout << "Access denied";
-	}
-		 
+int main(){
+    int age;
+    std::cin >> age;
+    if(ageCheck(age)){
+        std::cout << "Access granted";
+    }
+    else{
+        std::cout << "Access denied";
+    }
 }
-

f5eebded9705923e5fafd31b06463f5b200d1316
Artem Kuryakov <kurik01tagil@gmail.com>
2019-10-18 14:46:40 +0300

    Fixed task1

---
+++ src/main1.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+#include "task1.h"
+int main()
+{
+	int a;
+   std:: cin >> a;  
+  
+	   if (ageCheck(a))
+		   std::cout << "Access granted" << std::endl;
+	   else
+		   std::cout << "Access denied" << std::endl;
+   
+}
+

7acadf62f472c1e1a1ae9b27d379a0aebd6e7960
Andrew who don't know programming <agbazarkin@edu.hse.ru>
2019-11-26 23:33:16 +0300

    version 3

--- include/task1.h
+++ include/task1.h
@@ -1,4 +1,5 @@
+#pragma once
 #include <stdio.h>
 
 
-ageCheck(int age);
+int ageCheck(int age);
--- src/main1.cpp
+++ src/main1.cpp
@@ -1,12 +1,13 @@
 #include "task1.h"
 
 
-void main(){
+int main(){
 	int age;
-	printf("Pleasem enter your age here: ");
+	printf("Please enter your age here: ");
 	scanf("%d", &age);
 	if (ageCheck(age) == 1)
 		printf("Access granted");
 	else
 		printf("Access denied");
+	return 0;
 }

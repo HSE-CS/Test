ffcc97ada9cec99fe6ae9ef8ef70f8fe0a1fbc1b
Stifeev Nikita <stifeev.nikita@mail.ru>
2020-10-14 20:58:45 +0500

    b65 test

---
+++ src/main1.cpp
@@ -0,0 +1,14 @@
+﻿#include "task1.h"
+
+int main() {
+	int age;
+	cout << "Enter your age" << endl;
+	cin >> age;
+	if (ageCheck(age)) {
+		cout << "Access granted" << endl;
+	}
+	else {
+		cout << "Access denied" << endl;
+	}
+	return 0;
+}

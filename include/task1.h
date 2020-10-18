fc7f64df75ee38ceede3a854b15d2998ef92ca86
Karcevamd <mashkarcev@gmail.com>
2020-10-18 22:18:44 +0300

    19AMI Masha Kartseva

---
+++ include/task1.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef TASK1_H
+#define TASK1_H
+int ageCheck(int age);
+#endif // !TASK1_H
---
+++ src/task1.cpp
@@ -0,0 +1,6 @@
+int ageCheck(int age) {
+	int k = 0;
+	if (age >= 18)
+		k++;
+	return k;
+}
